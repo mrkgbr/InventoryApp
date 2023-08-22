7e8bceb3174282521b2587231fd1ac4d4dbf2841
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-08-22 12:42:02 +0200

    add inventory route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var inventoryRouter = require("./routes/inventory");
 
 require("dotenv").config();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/inventory", inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
