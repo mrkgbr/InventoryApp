fe6bc4c17f45bb04eee869c5f0f72e30e12fd784
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-08-23 07:18:55 +0200

    add item listing

---
+++ controllers/itemController.js
@@ -0,0 +1,14 @@
+const Item = require("../models/item");
+const Brand = require("../models/brand");
+const Category = require("../models/category");
+
+const asyncHandler = require("express-async-handler");
+
+exports.item_list = asyncHandler(async (req, res, next) => {
+  const allItems = await Item.find({}, "name brand")
+    .sort({ name: 1 })
+    .populate("brand")
+    .exec();
+  console.log(allItems);
+  res.render("item_list", { title: "Item List", list_item: allItems });
+});
--- routes/inventory.js
+++ routes/inventory.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 const category_controller = require("../controllers/categoryController");
 const brand_controller = require("../controllers/brandController");
+const item_controller = require("../controllers/itemController");
 
 // CATEGORY ROUTES //
 router.get("/categories", category_controller.category_list);
 
 // BRAND ROUTES //
 router.get("/brands", brand_controller.brand_list);
+
+// ITEM ROUTES //
+router.get("/items", item_controller.item_list);
 
 module.exports = router;
