bd18465512a98b74b051142b149702fa5c7386b1
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-08-28 11:07:27 +0200

    Add brand detail controller.

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,4 +1,5 @@
 const Brand = require("../models/brand");
+const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Category.
@@ -7,5 +8,25 @@ exports.brand_list = asyncHandler(async (req, res, next) => {
   res.render("brand_list", {
     title: "Brand List",
     list_brands: allBrands,
+  });
+});
+
+exports.brand_detail = asyncHandler(async (req, res, next) => {
+  const [brand, itemsInBrand] = await Promise.all([
+    Brand.findById(req.params.id).exec(),
+    Item.find({ brand: req.params.id }).exec(),
+  ]);
+
+  if (brand === null) {
+    // no result
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("brand_detail", {
+    title: "Brand detail",
+    brand: brand,
+    brand_items: itemsInBrand,
   });
 });
