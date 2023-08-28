327ed4d6b64ac81533a811a1578598adf523296e
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-08-28 10:15:32 +0200

    Add detail controller.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Category.
@@ -7,5 +8,26 @@ exports.category_list = asyncHandler(async (req, res, next) => {
   res.render("category_list", {
     title: "Category List",
     list_categories: allCategories,
+  });
+});
+
+// Display detail page for a specific Category
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const [category, itemInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }).exec(),
+  ]);
+
+  if (category === null) {
+    // No results.
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("category_detail", {
+    title: "Category detail",
+    category: category,
+    category_items: itemInCategory,
   });
 });
