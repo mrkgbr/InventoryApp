6cf850144ce62c550db75a1ceb9151bbecb4477e
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-08-31 10:47:05 +0200

    Add create item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const Item = require("../models/item");
 const Brand = require("../models/brand");
 const Category = require("../models/category");
@@ -36,4 +37,60 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
     brands: allBrands,
     categories: allCategories,
   });
+});
+
+exports.item_create_post = asyncHandler(async (req, res, next) => {
+  // Validate and sanitize fields.
+  body("name", "Title must not be empty.").trim().isLength({ min: 1 }).escape();
+  body("description", "Author must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape();
+  body("price", "Summary must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape();
+  body("brand.*").escape();
+  body("category.*").escape();
+  // Process request after validation and sanitization.
+
+  // Extract the validation errors from a request.
+  const errors = validationResult(req);
+
+  // Create a Book object with escaped and trimmed data.
+  const item = new Item({
+    name: req.body.product,
+    description: req.body.description,
+    price: req.body.price,
+    number_of_stock: req.body.numberOfStock,
+    brand: req.body.brand,
+    category: req.body.category,
+  });
+
+  if (!errors.isEmpty()) {
+    // There are errors. Render form again with sanitized values/error messages.
+    // Get all authors and genres for form.
+    const [allBrands, allCategories] = await Promise.all([
+      Brand.find().exec(),
+      Category.find().exec(),
+    ]);
+    // Mark our selected genres as checked.
+    for (const category of allCategories) {
+      if (item.category.includes(category._id)) {
+        // Current genre is selected. Set "checked" flag.
+        category.checked = "true";
+      }
+    }
+    res.render("item_form", {
+      title: "Create Item",
+      brand: allBrands,
+      category: allCategories,
+      item: item,
+      errors: errors.array(),
+    });
+  } else {
+    // Data from form is valid. Save book.
+    await item.save();
+    res.redirect(item.url);
+  }
 });
