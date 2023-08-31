f78b045c6eb87c09848d53d637acfa225a042b00
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-08-31 10:07:35 +0200

    Add item create get function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Item = require("../models/item");
-// const Brand = require("../models/brand");
-// const Category = require("../models/category");
+const Brand = require("../models/brand");
+const Category = require("../models/category");
 
 exports.item_list = asyncHandler(async (req, res, next) => {
   const allItems = await Item.find({}, "name brand")
@@ -16,5 +16,24 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
     .populate("brand")
     .populate("category")
     .exec();
+
+  if (item === null) {
+    const err = new Error("Item not found");
+    err.status = 404;
+    return next(err);
+  }
   res.render("item_detail", { title: "Item Detail", detail_item: item });
+});
+
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+  const [allBrands, allCategories] = await Promise.all([
+    Brand.find().exec(),
+    Category.find().exec(),
+  ]);
+
+  res.render("item_form", {
+    title: "Create Item",
+    brands: allBrands,
+    categories: allCategories,
+  });
 });
--- routes/inventory.js
+++ routes/inventory.js
@@ -22,6 +22,7 @@ router.get("/brand/:id", brand_controller.brand_detail);
 
 // ITEM ROUTES //
 router.get("/items", item_controller.item_list);
+router.get("/item/create", item_controller.item_create_get);
 router.get("/item/:id", item_controller.item_detail);
 
 module.exports = router;
